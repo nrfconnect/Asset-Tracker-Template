03c53e909c79d6120605b259b313ba4f975015c6
Giacomo Dematteis <giacomo.dematteis@nordicsemi.no>
2025-01-13 16:16:59 +0100

    tests: on_target: fix some residual dfu garbage
    Removing some dfu garbage.
    
    Signed-off-by: Giacomo Dematteis <giacomo.dematteis@nordicsemi.no>

--- tests/on_target/utils/flash_tools.py
+++ tests/on_target/utils/flash_tools.py
@@ -9,7 +9,6 @@ import sys
 import glob
 sys.path.append(os.getcwd())
 from utils.logger import get_logger
-from utils.thingy91x_dfu import detect_family_from_zip
 
 logger = get_logger()
 
@@ -59,67 +58,6 @@ def recover_device(serial=SEGGER, core="Application"):
         logger.info("An error occurred while recovering the device.")
         logger.info("Error output:")
         logger.info(e.stderr)
-        raise
-
-def dfu_device(zipfile, serial=None, reset_only=False, check_53_version=False, bootloader_slot=1):
-    chip, is_mcuboot = detect_family_from_zip(zipfile)
-    if chip is None:
-        logger.error("Could not determine chip family from image")
-        raise ValueError("Invalid image file")
-    command = [
-        'python3',
-        'utils/thingy91x_dfu.py',
-        '--image', zipfile,
-        '--chip', chip,
-        '--bootloader-slot', str(bootloader_slot)
-    ]
-
-    if serial:
-        command.append('--serial')
-        command.append(serial)
-    if reset_only:
-        command.append('--reset-only')
-    if check_53_version:
-        command.append('--check-nrf53-version')
-
-    try:
-        result = subprocess.run(command, check=True, text=True, capture_output=True)
-        logger.info("Output from dfu script:")
-        logger.info(result.stdout)
-        return result.stdout
-    except subprocess.CalledProcessError as e:
-        logger.error("Error from dfu script:")
-        logger.error(e.stderr)
-        raise e
-
-def setup_jlink(serial_number):
-    """
-    Flash Segger firmware to nRF53 on Thingy91x through external debugger.
-
-    Args:
-        serial_number (str): Serial number of the external debugger.
-
-    Raises:
-        subprocess.CalledProcessError: If the setup-jlink command fails.
-    """
-    logger.info("Flashing Segger firmware to nRF53 on Thingy91x through external debugger")
-
-    setup_jlink_command = ['/opt/setup-jlink/setup-jlink.bash', serial_number]
-
-    try:
-        subprocess.run(
-            setup_jlink_command,
-            check=True,
-            text=True,
-            capture_output=True
-        )
-        logger.info("Segger firmware flashed successfully.")
-    except subprocess.CalledProcessError as e:
-        logger.error("Failed to flash Segger firmware.")
-        logger.error(f"Command: {' '.join(setup_jlink_command)}")
-        logger.error(f"Exit code: {e.returncode}")
-        logger.error(f"Standard output:\n{e.stdout}")
-        logger.error(f"Standard error:\n{e.stderr}")
         raise
 
 def get_first_artifact_match(pattern):
