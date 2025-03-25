ee7719d6eb378f70f6515b240d8233bbc3907042
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2025-03-25 10:39:53 +0100

    tests: main: Add test cases for FOTA states
    - Add testcases for FOTA states
    - Rename check_* helper functions to expect_*
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

--- tests/module/main/src/checks.c
+++ tests/module/main/src/checks.c
@@ -12,17 +12,20 @@
 #include "power.h"
 #include "network.h"
 #include "checks.h"
+#include "fota.h"
 
+ZBUS_MSG_SUBSCRIBER_DEFINE(fota_subscriber);
 ZBUS_MSG_SUBSCRIBER_DEFINE(location_subscriber);
 ZBUS_MSG_SUBSCRIBER_DEFINE(network_subscriber);
 ZBUS_MSG_SUBSCRIBER_DEFINE(power_subscriber);
+ZBUS_CHAN_ADD_OBS(FOTA_CHAN, fota_subscriber, 0);
 ZBUS_CHAN_ADD_OBS(LOCATION_CHAN, location_subscriber, 0);
 ZBUS_CHAN_ADD_OBS(NETWORK_CHAN, network_subscriber, 0);
 ZBUS_CHAN_ADD_OBS(POWER_CHAN, power_subscriber, 0);
 
 LOG_MODULE_REGISTER(main_module_checks, 4);
 
-void check_location_event(enum location_msg_type expected_location_type)
+void expect_location_event(enum location_msg_type expected_location_type)
 {
 	int err;
 	const struct zbus_channel *chan;
@@ -51,7 +54,7 @@ void check_location_event(enum location_msg_type expected_location_type)
 	TEST_ASSERT_EQUAL(expected_location_type, location_msg_type);
 }
 
-void check_network_event(enum network_msg_type expected_network_type)
+void expect_network_event(enum network_msg_type expected_network_type)
 {
 	int err;
 	const struct zbus_channel *chan;
@@ -80,7 +83,7 @@ void check_network_event(enum network_msg_type expected_network_type)
 	TEST_ASSERT_EQUAL(expected_network_type, network_msg.type);
 }
 
-void check_power_event(enum power_msg_type expected_power_type)
+void expect_power_event(enum power_msg_type expected_power_type)
 {
 	int err;
 	const struct zbus_channel *chan;
@@ -109,8 +112,36 @@ void check_power_event(enum power_msg_type expected_power_type)
 	TEST_ASSERT_EQUAL(expected_power_type, power_msg.type);
 }
 
+void expect_fota_event(enum fota_msg_type expected_fota_type)
+{
+	int err;
+	const struct zbus_channel *chan;
+	enum fota_msg_type fota_msg_type;
 
-static void check_no_location_events(void)
+	/* Allow the test thread to sleep so that the DUT's thread is allowed to run. */
+	k_sleep(K_MSEC(100));
+
+	err = zbus_sub_wait_msg(&fota_subscriber, &chan, &fota_msg_type, K_MSEC(10000));
+	if (err == -ENOMSG) {
+		LOG_ERR("No FOTA event received");
+		TEST_FAIL();
+	} else if (err) {
+		LOG_ERR("zbus_sub_wait, error: %d", err);
+		TEST_FAIL();
+
+		return;
+	}
+
+	if (chan != &FOTA_CHAN) {
+		LOG_ERR("Received message from wrong channel, expected %s, got %s",
+			zbus_chan_name(&FOTA_CHAN), zbus_chan_name(chan));
+		TEST_FAIL();
+	}
+
+	TEST_ASSERT_EQUAL(expected_fota_type, fota_msg_type);
+}
+
+static void expect_no_location_events(void)
 {
 	int err;
 	const struct zbus_channel *chan;
@@ -130,7 +161,7 @@ static void check_no_location_events(void)
 	TEST_FAIL();
 }
 
-static void check_no_network_events(void)
+static void expect_no_network_events(void)
 {
 	int err;
 	const struct zbus_channel *chan;
@@ -150,7 +181,7 @@ static void check_no_network_events(void)
 	TEST_FAIL();
 }
 
-static void check_no_power_events(void)
+static void expect_no_power_events(void)
 {
 	int err;
 	const struct zbus_channel *chan;
@@ -170,13 +201,34 @@ static void check_no_power_events(void)
 	TEST_FAIL();
 }
 
-void check_no_events(uint32_t timeout_sec)
+static void expect_no_fota_events(void)
+{
+	int err;
+	const struct zbus_channel *chan;
+	enum fota_msg_type fota_msg_type;
+
+	err = zbus_sub_wait_msg(&fota_subscriber, &chan, &fota_msg_type, K_MSEC(10000));
+	if (err == -ENOMSG) {
+		return;
+	} else if (err) {
+		LOG_ERR("zbus_sub_wait, error: %d", err);
+		TEST_FAIL();
+
+		return;
+	}
+
+	LOG_ERR("Received unexpected FOTA event: %d", fota_msg_type);
+	TEST_FAIL();
+}
+
+void expect_no_events(uint32_t timeout_sec)
 {
 	k_sleep(K_SECONDS(timeout_sec));
 
-	check_no_location_events();
-	check_no_network_events();
-	check_no_power_events();
+	expect_no_location_events();
+	expect_no_network_events();
+	expect_no_power_events();
+	expect_no_fota_events();
 }
 
 void purge_location_events(void)
@@ -237,11 +289,33 @@ void purge_power_events(void)
 	}
 }
 
+void purge_fota_events(void)
+{
+	while (true) {
+		int err;
+		const struct zbus_channel *chan;
+		enum fota_msg_type fota_msg_type;
+
+		err = zbus_sub_wait_msg(&fota_subscriber, &chan, &fota_msg_type, K_MSEC(100));
+		if (err == -ENOMSG) {
+			break;
+		} else if (err) {
+			LOG_ERR("zbus_sub_wait, error: %d", err);
+			TEST_FAIL();
+
+			return;
+		}
+	}
+}
+
 void purge_all_events(void)
 {
+	k_sleep(K_SECONDS(1));
+
 	purge_location_events();
 	purge_network_events();
 	purge_power_events();
+	purge_fota_events();
 }
 
 int wait_for_location_event(enum location_msg_type expected_type, uint32_t timeout_sec)
