0f43b54012a976c9b7ef9f21b5bb8068ef851641
Simen S. Røstad <simen.rostad@nordicsemi.no>
2025-04-28 13:05:26 +0200

    docs: Add support for MQTT cloud example module
    Add support for MQTT cloud example module
    
    Signed-off-by: Simen S. Røstad <simen.rostad@nordicsemi.no>

---
+++ examples/modules/cloud/creds.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+static const unsigned char ca_certificate[] = {
+#include ATT_MQTT_CA_CERT
+(0x00)
+};
