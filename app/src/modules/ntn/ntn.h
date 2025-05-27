1d3ac858af36c342d7a4c41b66c9c268a6c0b7d8
Giacomo Dematteis <giacomo.dematteis@nordicsemi.no>
2025-05-27 14:29:43 +0200

    ntn: ntn usecase support
    Add experimental support for simple NTN usecase.
    
    Signed-off-by: Giacomo Dematteis <giacomo.dematteis@nordicsemi.no>

---
+++ app/src/modules/ntn/ntn.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef NTN_H
+#define NTN_H
+
+#include <zephyr/kernel.h>
+#include <zephyr/zbus/zbus.h>
+#include <nrf_modem_gnss.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* NTN module message types */
+enum ntn_msg_type {
+	/* Events that trigger state transitions */
+	NTN_LOCATION_SEARCH_DONE, /* Location search completed - triggers transition to NTN mode */
+	NTN_TIMEOUT,              /* NTN mode timeout occurred - triggers transition to GNSS mode */
+	NTN_NETWORK_CONNECTED,    /* Network connectivity established */
+	GNSS_SEARCH_FAILED,       /* GNSS search failed */
+};
+
+/* NTN module message */
+struct ntn_msg {
+	enum ntn_msg_type type;
+	struct nrf_modem_gnss_pvt_data_frame pvt;
+};
+
+/* Declare the NTN message channel */
+ZBUS_CHAN_DECLARE(NTN_CHAN);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NTN_H */
