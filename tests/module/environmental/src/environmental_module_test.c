b2b96ed51847ae7c683aa25da2ababf1f0f3c538
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2025-07-04 12:30:02 +0200

    tests: environmental: Rename source file
    Rename from main.c to environmental_module_test.c to align with
    other modules.
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

--- tests/module/environmental/src/environmental_module_test.c
+++ tests/module/environmental/src/environmental_module_test.c
@@ -8,6 +8,7 @@
 #include <zephyr/zbus/zbus.h>
 #include <zephyr/task_wdt/task_wdt.h>
 #include <zephyr/logging/log.h>
+#include <date_time.h>
 
 #include "app_common.h"
 #include "environmental.h"
@@ -16,17 +17,37 @@ DEFINE_FFF_GLOBALS;
 
 FAKE_VALUE_FUNC(int, task_wdt_feed, int);
 FAKE_VALUE_FUNC(int, task_wdt_add, uint32_t, task_wdt_callback_t, void *);
+FAKE_VALUE_FUNC(int, date_time_now, int64_t *);
 
 ZBUS_MSG_SUBSCRIBER_DEFINE(environmental_subscriber);
 ZBUS_CHAN_ADD_OBS(ENVIRONMENTAL_CHAN, environmental_subscriber, 0);
 
 LOG_MODULE_REGISTER(environmental_module_test, 4);
 
+/* Test timestamp value to be returned by date_time_now mock */
+static int64_t test_timestamp = 1640995200000; /* 2022-01-01 00:00:00 UTC in ms */
+
+static int date_time_now_custom_fake(int64_t *timestamp)
+{
+	*timestamp = test_timestamp;
+	return 0;
+}
+
+static int date_time_now_error_fake(int64_t *timestamp)
+{
+	/* Return error, timestamp should remain 0 */
+	return -ENODATA;
+}
+
 void setUp(void)
 {
 	/* reset fakes */
 	RESET_FAKE(task_wdt_feed);
 	RESET_FAKE(task_wdt_add);
+	RESET_FAKE(date_time_now);
+
+	/* Set up custom fake function for date_time_now */
+	date_time_now_fake.custom_fake = date_time_now_custom_fake;
 }
 
 void check_environmental_event(enum environmental_msg_type expected_environmental_type)
@@ -55,6 +76,12 @@ void check_environmental_event(enum environmental_msg_type expected_environmenta
 	}
 
 	TEST_ASSERT_EQUAL(expected_environmental_type, environmental_msg.type);
+
+	/* Verify timestamp for response messages */
+	if (expected_environmental_type == ENVIRONMENTAL_SENSOR_SAMPLE_RESPONSE) {
+		TEST_ASSERT_EQUAL(test_timestamp, environmental_msg.timestamp);
+		LOG_DBG("Timestamp verification passed: %lld", environmental_msg.timestamp);
+	}
 }
 
 void check_no_environmental_events(uint32_t time_in_seconds)
@@ -82,6 +109,70 @@ static void send_environmental_sample_request(void)
 	int err = zbus_chan_pub(&ENVIRONMENTAL_CHAN, &msg, K_SECONDS(1));
 
 	TEST_ASSERT_EQUAL(0, err);
+}
+
+void test_timestamp_verification(void)
+{
+	/* Test with different timestamp values */
+	int64_t test_timestamps[] = {
+		1640995200000, /* 2022-01-01 00:00:00 UTC in ms */
+		1672531200000, /* 2023-01-01 00:00:00 UTC in ms */
+		1704067200000  /* 2024-01-01 00:00:00 UTC in ms */
+	};
+
+	for (int i = 0; i < ARRAY_SIZE(test_timestamps); i++) {
+		/* Set new timestamp value */
+		test_timestamp = test_timestamps[i];
+
+		/* Given */
+		send_environmental_sample_request();
+
+		/* When */
+		k_sleep(K_SECONDS(1));
+
+		/* Then */
+		check_environmental_event(ENVIRONMENTAL_SENSOR_SAMPLE_REQUEST);
+		check_environmental_event(ENVIRONMENTAL_SENSOR_SAMPLE_RESPONSE);
+
+		/* Verify that date_time_now was called */
+		TEST_ASSERT_GREATER_THAN(0, date_time_now_fake.call_count);
+
+		/* Reset for next iteration */
+		RESET_FAKE(date_time_now);
+		date_time_now_fake.custom_fake = date_time_now_custom_fake;
+	}
+}
+
+void test_timestamp_error_handling(void)
+{
+	/* Test error handling when date_time_now fails */
+
+	/* Set up error fake */
+	date_time_now_fake.custom_fake = date_time_now_error_fake;
+
+	/* Given */
+	send_environmental_sample_request();
+
+	/* When */
+	k_sleep(K_SECONDS(1));
+
+	/* Then */
+	check_environmental_event(ENVIRONMENTAL_SENSOR_SAMPLE_REQUEST);
+
+	/* For error case, we expect timestamp to be 0 */
+	int err;
+	const struct zbus_channel *chan;
+	struct environmental_msg environmental_msg;
+
+	k_sleep(K_MSEC(100));
+
+	err = zbus_sub_wait_msg(&environmental_subscriber, &chan, &environmental_msg, K_MSEC(1000));
+	TEST_ASSERT_EQUAL(0, err);
+	TEST_ASSERT_EQUAL(ENVIRONMENTAL_SENSOR_SAMPLE_RESPONSE, environmental_msg.type);
+	TEST_ASSERT_EQUAL(0, environmental_msg.timestamp); /* Should be 0 on error */
+
+	/* Verify that date_time_now was called */
+	TEST_ASSERT_GREATER_THAN(0, date_time_now_fake.call_count);
 }
 
 void test_sensor_sample(void)
