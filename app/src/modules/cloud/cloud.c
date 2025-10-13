7c12b5122aa718a528dfe49a51634f3a0113882f
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2025-10-13 14:09:56 +0200

    modules: cloud: Move location code to separate file
    Move the location related code to a separate file that is only
    compiled if location is enabled.
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

--- app/src/modules/cloud/cloud.c
+++ app/src/modules/cloud/cloud.c
@@ -24,7 +24,9 @@
 #endif /* CONFIG_MEMFAULT */
 
 #include "cloud.h"
+#include "cloud_internal.h"
 #include "cloud_provisioning.h"
+#include "cloud_location.h"
 #include "app_common.h"
 #include "network.h"
 #include "storage.h"
@@ -81,17 +83,6 @@ ZBUS_CHAN_DEFINE(CLOUD_CHAN,
 		 ZBUS_OBSERVERS_EMPTY,
 		 ZBUS_MSG_INIT(.type = CLOUD_DISCONNECTED)
 );
-
-/* Enumerator to be used in privat cloud channel */
-enum priv_cloud_msg {
-	CLOUD_CONNECTION_FAILED,
-	CLOUD_CONNECTION_SUCCESS,
-	CLOUD_NOT_AUTHENTICATED,
-	CLOUD_PROVISIONING_FINISHED,
-	CLOUD_PROVISIONING_FAILED,
-	CLOUD_BACKOFF_EXPIRED,
-	CLOUD_SEND_REQUEST_FAILED,
-};
 
 /* Create private cloud channel for internal messaging that is not intended for external use.
  * The channel is needed to communicate from asynchronous callbacks to the state machine and
@@ -194,10 +185,6 @@ static enum smf_state_result state_connected_ready_run(void *obj);
 static void state_connected_paused_entry(void *obj);
 static enum smf_state_result state_connected_paused_run(void *obj);
 
-/* Forward declarations of location handler */
-#if defined(CONFIG_APP_LOCATION)
-static void handle_location_message(const struct location_msg *msg);
-#endif
 
 /* Forward declarations of network handler used before its definition */
 #if defined(CONFIG_APP_NETWORK)
@@ -437,7 +424,7 @@ static int send_storage_data_to_cloud(const struct storage_data_item *item)
 	if (item->type == STORAGE_TYPE_LOCATION) {
 		const struct location_msg *loc = &item->data.LOCATION;
 
-		handle_location_message(loc);
+		cloud_location_handle_message(loc);
 
 		return 0;
 	}
@@ -905,162 +892,6 @@ static void state_connected_exit(void *obj)
 	}
 }
 
-#if defined(CONFIG_APP_LOCATION)
-/* Handle cloud location requests from the location module */
-static void handle_cloud_location_request(const struct location_data_cloud *request)
-{
-	int err;
-	struct nrf_cloud_location_config loc_config = {
-		.do_reply = false,
-	};
-	struct nrf_cloud_rest_location_request loc_req = {
-		.config = &loc_config,
-	};
-	struct nrf_cloud_location_result result = { 0 };
-
-	LOG_DBG("Handling cloud location request");
-
-#if defined(CONFIG_LOCATION_METHOD_CELLULAR)
-	if (request->cell_data != NULL) {
-		loc_req.cell_info = (struct lte_lc_cells_info *)request->cell_data;
-
-		LOG_DBG("Cellular data present: current cell ID: %d, neighbor cells: %d, "
-			"GCI cells count: %d",
-			request->cell_data->current_cell.id,
-			request->cell_data->ncells_count,
-			request->cell_data->gci_cells_count);
-	}
-#endif
-
-#if defined(CONFIG_LOCATION_METHOD_WIFI)
-	if (request->wifi_data != NULL) {
-		loc_req.wifi_info = (struct wifi_scan_info *)request->wifi_data;
-
-		LOG_DBG("Wi-Fi data present: %d APs", request->wifi_data->cnt);
-	}
-#endif
-
-	/* Send location request to nRF Cloud */
-	err = nrf_cloud_coap_location_get(&loc_req, &result);
-	if (err == COAP_RESPONSE_CODE_NOT_FOUND) {
-		LOG_WRN("nRF Cloud CoAP location coordinates not found, error: %d", err);
-		location_cloud_location_ext_result_set(LOCATION_EXT_RESULT_ERROR, NULL);
-
-		return;
-	} else if (err) {
-		LOG_ERR("nrf_cloud_coap_location_get, error: %d", err);
-		location_cloud_location_ext_result_set(LOCATION_EXT_RESULT_ERROR, NULL);
-
-		send_request_failed();
-		return;
-	}
-}
-
-#if defined(CONFIG_NRF_CLOUD_AGNSS)
-/* Handle A-GNSS data requests from the location module */
-static void handle_agnss_request(const struct nrf_modem_gnss_agnss_data_frame *request)
-{
-	int err;
-	static char agnss_buf[AGNSS_MAX_DATA_SIZE];
-	struct nrf_cloud_rest_agnss_request agnss_req = {
-		.type = NRF_CLOUD_REST_AGNSS_REQ_CUSTOM,
-		.agnss_req = (struct nrf_modem_gnss_agnss_data_frame *)request,
-		.net_info = NULL,
-		.filtered = false,
-		.mask_angle = 0
-	};
-	struct nrf_cloud_rest_agnss_result result = {
-		.buf = agnss_buf,
-		.buf_sz = sizeof(agnss_buf),
-		.agnss_sz = 0
-	};
-
-	LOG_DBG("Handling A-GNSS data request");
-
-	/* Send A-GNSS request to nRF Cloud */
-	err = nrf_cloud_coap_agnss_data_get(&agnss_req, &result);
-	if (err) {
-		LOG_ERR("nrf_cloud_coap_agnss_data_get, error: %d", err);
-
-		send_request_failed();
-		return;
-	}
-
-	LOG_DBG("A-GNSS data received, size: %d bytes", result.agnss_sz);
-
-	/* Process the A-GNSS data */
-	err = location_agnss_data_process(result.buf, result.agnss_sz);
-	if (err) {
-		LOG_ERR("Failed to process A-GNSS data, error: %d", err);
-		return;
-	}
-
-	LOG_DBG("A-GNSS data processed successfully");
-}
-#endif /* CONFIG_NRF_CLOUD_AGNSS */
-
-#if defined(CONFIG_LOCATION_METHOD_GNSS)
-/* Handle GNSS location data from the location module */
-static void handle_gnss_location_data(const struct location_data *location_data)
-{
-	int err;
-	int64_t timestamp_ms = NRF_CLOUD_NO_TIMESTAMP;
-	bool confirmable = IS_ENABLED(CONFIG_APP_CLOUD_CONFIRMABLE_MESSAGES);
-	struct nrf_cloud_gnss_data gnss_data = {
-		.type = NRF_CLOUD_GNSS_TYPE_PVT,
-		.ts_ms = timestamp_ms,
-		.pvt = {
-			.lat = location_data->latitude,
-			.lon = location_data->longitude,
-			.accuracy = location_data->accuracy,
-		}
-	};
-
-	LOG_DBG("Handling GNSS location data: lat: %f, lon: %f, acc: %f",
-		(double)location_data->latitude,
-		(double)location_data->longitude,
-		(double)location_data->accuracy);
-
-	/* Get current timestamp */
-	err = date_time_now(&timestamp_ms);
-	if (err) {
-		LOG_WRN("Failed to get current time");
-
-		timestamp_ms = NRF_CLOUD_NO_TIMESTAMP;
-	}
-
-	gnss_data.ts_ms = timestamp_ms;
-
-#if defined(CONFIG_LOCATION_DATA_DETAILS)
-#define CLOUD_GNSS_HEADING_ACC_LIMIT (float)60.0
-
-	struct location_data_details_gnss gnss = location_data->details.gnss;
-
-	/* If detailed GNSS data is available, include altitude, speed, and heading */
-	if (gnss.pvt_data.flags & NRF_MODEM_GNSS_PVT_FLAG_FIX_VALID) {
-		gnss_data.pvt.alt = gnss.pvt_data.altitude;
-		gnss_data.pvt.speed = gnss.pvt_data.speed;
-		gnss_data.pvt.heading = gnss.pvt_data.heading;
-		gnss_data.pvt.has_alt = 1;
-		gnss_data.pvt.has_speed =
-			(gnss.pvt_data.flags & NRF_MODEM_GNSS_PVT_FLAG_VELOCITY_VALID) ? 1 : 0;
-		gnss_data.pvt.has_heading =
-			(gnss.pvt_data.heading_accuracy < CLOUD_GNSS_HEADING_ACC_LIMIT) ? 1 : 0;
-	}
-#endif /* CONFIG_LOCATION_DATA_DETAILS */
-
-	/* Send GNSS location data to nRF Cloud */
-	err = nrf_cloud_coap_location_send(&gnss_data, confirmable);
-	if (err) {
-		LOG_ERR("nrf_cloud_coap_location_send, error: %d", err);
-		send_request_failed();
-		return;
-	}
-
-	LOG_INF("GNSS location data sent to nRF Cloud successfully");
-}
-#endif /* CONFIG_LOCATION_METHOD_GNSS */
-#endif /* CONFIG_APP_LOCATION */
 
 static void shadow_poll(enum shadow_poll_type type)
 {
@@ -1197,34 +1028,6 @@ static void handle_network_data_message(const struct network_msg *msg)
 }
 #endif /* CONFIG_APP_NETWORK */
 
-#if defined(CONFIG_APP_LOCATION)
-static void handle_location_message(const struct location_msg *msg)
-{
-	switch (msg->type) {
-	case LOCATION_CLOUD_REQUEST:
-		LOG_DBG("Cloud location request received");
-		handle_cloud_location_request(&msg->cloud_request);
-		break;
-
-#if defined(CONFIG_NRF_CLOUD_AGNSS)
-	case LOCATION_AGNSS_REQUEST:
-		LOG_DBG("A-GNSS data request received");
-		handle_agnss_request(&msg->agnss_request);
-		break;
-#endif /* CONFIG_NRF_CLOUD_AGNSS */
-
-#if defined(CONFIG_LOCATION_METHOD_GNSS)
-	case LOCATION_GNSS_DATA:
-		LOG_DBG("GNSS location data received");
-		handle_gnss_location_data(&msg->gnss_data);
-		break;
-#endif /* CONFIG_LOCATION_METHOD_GNSS */
-
-	default:
-		break;
-	}
-}
-#endif /* CONFIG_APP_LOCATION */
 
 static void handle_storage_channel_message(struct cloud_state_object const *state_object)
 {
@@ -1322,18 +1125,6 @@ static enum smf_state_result state_connected_ready_run(void *obj)
 		return SMF_EVENT_HANDLED;
 	}
 
-	if (state_object->chan == &LOCATION_CHAN) {
-		const struct location_msg *msg = MSG_TO_LOCATION_MSG_PTR(state_object->msg_buf);
-
-		if (msg->type == LOCATION_AGNSS_REQUEST) {
-			LOG_DBG("A-GNSS data request received");
-
-			handle_agnss_request(&msg->agnss_request);
-		}
-
-		return SMF_EVENT_HANDLED;
-	}
-
 	if (state_object->chan == &STORAGE_CHAN) {
 		handle_storage_channel_message(state_object);
 
@@ -1351,6 +1142,20 @@ static enum smf_state_result state_connected_ready_run(void *obj)
 
 		return SMF_EVENT_HANDLED;
 	}
+
+#if defined(CONFIG_APP_LOCATION)
+	if (state_object->chan == &LOCATION_CHAN) {
+		const struct location_msg *msg = MSG_TO_LOCATION_MSG_PTR(state_object->msg_buf);
+
+		if (msg->type == LOCATION_AGNSS_REQUEST) {
+			LOG_DBG("A-GNSS data request received");
+
+			cloud_location_handle_message(msg);
+		}
+
+		return SMF_EVENT_HANDLED;
+	}
+#endif /* CONFIG_APP_LOCATION */
 
 	return SMF_EVENT_PROPAGATE;
 }
