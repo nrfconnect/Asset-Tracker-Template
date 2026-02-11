09d1c27e9b775ec90a818f513545e85512ed349e
Trond F. Christiansen <trond.christiansen@nordicsemi.no>
2026-02-11 12:54:29 +0100

    modules: fix MSG_TO_STORAGE_MSG macro
    Rename MSG_TO_STORAGE_MSG to MSG_TO_STORAGE_MSG_PTR to make behavior
    clearer and matching similar macros in other modules.
    
    Signed-off-by: Trond F. Christiansen <trond.christiansen@nordicsemi.no>

--- app/src/main.c
+++ app/src/main.c
@@ -998,7 +998,7 @@ static enum smf_state_result buffer_disconnected_run(void *o)
 	}
 
 	if (state_object->chan == &STORAGE_CHAN) {
-		const struct storage_msg *msg = MSG_TO_STORAGE_MSG(state_object->msg_buf);
+		const struct storage_msg *msg = MSG_TO_STORAGE_MSG_PTR(state_object->msg_buf);
 
 		if (msg->type == STORAGE_MODE_PASSTHROUGH) {
 			smf_set_state(SMF_CTX(state_object),
@@ -1085,7 +1085,7 @@ static enum smf_state_result buffer_connected_run(void *o)
 	}
 
 	if (state_object->chan == &STORAGE_CHAN) {
-		const struct storage_msg *msg = MSG_TO_STORAGE_MSG(state_object->msg_buf);
+		const struct storage_msg *msg = MSG_TO_STORAGE_MSG_PTR(state_object->msg_buf);
 
 		if (msg->type == STORAGE_MODE_PASSTHROUGH) {
 			smf_set_state(SMF_CTX(state_object),
@@ -1410,7 +1410,7 @@ static enum smf_state_result passthrough_disconnected_run(void *o)
 	}
 
 	if (state_object->chan == &STORAGE_CHAN) {
-		const struct storage_msg *msg = MSG_TO_STORAGE_MSG(state_object->msg_buf);
+		const struct storage_msg *msg = MSG_TO_STORAGE_MSG_PTR(state_object->msg_buf);
 
 		if (msg->type == STORAGE_MODE_BUFFER) {
 			smf_set_state(SMF_CTX(state_object), &states[STATE_BUFFER_DISCONNECTED]);
@@ -1473,7 +1473,7 @@ static enum smf_state_result passthrough_connected_run(void *o)
 	}
 
 	if (state_object->chan == &STORAGE_CHAN) {
-		const struct storage_msg *msg = MSG_TO_STORAGE_MSG(state_object->msg_buf);
+		const struct storage_msg *msg = MSG_TO_STORAGE_MSG_PTR(state_object->msg_buf);
 
 		if (msg->type == STORAGE_MODE_BUFFER) {
 			smf_set_state(SMF_CTX(state_object), &states[STATE_BUFFER_CONNECTED]);
