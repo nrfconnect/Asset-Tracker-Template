9b2befcffa4f77713d4537aa1f630e86aeb03081
Simen S. Røstad <simen.rostad@nordicsemi.no>
2026-06-18 10:36:31 +0200

    tests: on_target: test_functional: Revert expected string
    Revert expected log message in ci.
    
    Signed-off-by: Simen S. Røstad <simen.rostad@nordicsemi.no>

--- tests/on_target/tests/test_functional/test_buffer_flash.py
+++ tests/on_target/tests/test_functional/test_buffer_flash.py
@@ -69,9 +69,8 @@ def test_buffer_flash(dut_cloud, hex_file_buffer_flash):
         reset_device()
         start_pos = dut_cloud.uart.get_size()
 
-        # Wait for storage mount (Zephyr fstab automount logs via littlefs or lfs_backend)
         dut_cloud.uart.wait_for_str(
-            ["/att_storage mounted", "/att_storage already mounted"],
+            ["/att_storage already mounted"],
             timeout=60,
             start_pos=start_pos,
         )
